520ebc91e7d64198e5d96e6c258b956f817e96ff
allenflo <allenflo@outlook.com>
2022-09-18 19:42:30 -0700

    Added missing days.

--- Projects/2_3YourLifeInWeeks.py
+++ Projects/2_3YourLifeInWeeks.py
@@ -7,10 +7,9 @@ daysLeft = 365 * int(yearsLeft)
 weeksLeft = yearsLeft * 52
 monthsLeft = yearsLeft * 12
 
-
-
 print(f"You have {yearsLeft} years left to live")
-print(f"You have {weeksLeft} weeks left to live")
 print(f"You have {monthsLeft} months left to live")
+print(f"You have {weeksLeft} weeks left to live")
+print(f"You have {daysLeft} days left to live")
 
 
