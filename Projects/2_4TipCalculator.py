f03807fdaaab2d744218b9b75cd8e7e2f7da3c4f
allenflo <allenflo@outlook.com>
2022-09-18 23:08:35 -0700

    Create 2_4TipCalculator.py

---
+++ Projects/2_4TipCalculator.py
@@ -0,0 +1,16 @@
+#Tip calulator will
+#Welcome You
+#Ask for total bill amount
+#Ask what percentage tip to give?
+#Ask how may people to split the bill
+#Tell how much each person should pay
+
+print("Welcome to the Tip Calculator")
+
+print("Wecome to the tip calculator")
+total = input("What was the total bill? $")
+tip = input("What percent of tip would you like to give? %")
+split = input("How many peole to split the bill? ")
+
+
+
