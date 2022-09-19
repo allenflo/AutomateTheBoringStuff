129db63751f78d71d2b1119a4250e60068cb6d15
allenflo <allenflo@outlook.com>
2022-09-18 23:44:57 -0700

    Debugging

--- Projects/2_4TipCalculator.py
+++ Projects/2_4TipCalculator.py
@@ -9,8 +9,22 @@ print("Welcome to the Tip Calculator")
 
 print("Wecome to the tip calculator")
 total = input("What was the total bill? $")
-tip = input("What percent of tip would you like to give? %")
+percentTip = input("What percent of tip would you like to give? %")
 split = input("How many peole to split the bill? ")
 
 
+#Calculate tip amount
+tip = (int(percentTip) / 100) * float(total)
 
+
+#Calulate total bill amount
+totalBill = total + float(tip)
+
+#Split total bill
+each = totalBill / int(split)
+
+#Round to 2 decimals
+
+eachPay = float(round(each, 2))
+
+print(f"Each person should pay:  ${eachPay}")
