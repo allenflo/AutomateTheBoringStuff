42692ca9414ae27285e52d1e2f7c7978bc519523
allenflo <allenflo@outlook.com>
2022-09-21 00:00:15 -0700

    Update 5_4PasswordGenerator.py

--- Projects/5_4PasswordGenerator.py
+++ Projects/5_4PasswordGenerator.py
@@ -1,43 +1,31 @@
-#Password Generator Project
+#password_list Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+print("Welcome to the Pypassword_list Generator!")
+nr_letters= int(input("How many letters would you like in your password_list?\n")) 
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
+password_list = []
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+for alpha in range(1, nr_letters +1):
+    password_list += random.choice(letters)
 
+for num in range(1, nr_numbers +1):
+    password_list += random.choice(numbers)
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+for sym in range(1, nr_symbols +1):
+    password_list += random.choice(symbols)
 
-alphapass = ""
-numpass = ""
-sympass= ""
+random.shuffle(password_list)
 
-for alpha in range(0, (int(nr_letters))):
-    letter = random.randint(0,51)
-    alphapass = alphapass + letters[letter]
+password = ""
+for char in password_list:
+    password += char
 
-
-for num in range(0, (int(nr_numbers))):
-    number = random.randint(0,9)
-    numpass = numpass + numbers[number]
-
-
-for sym in range(0, (int(nr_symbols))):
-    symbol = random.randint(0,8)
-    sympass = sympass + symbols[symbol]
-
-
-newPassword = alphapass + numpass + sympass
-print(newPassword)
-
+print(password)
 
 
