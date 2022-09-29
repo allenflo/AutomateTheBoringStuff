2261308ee2976c429cf860a5026722f0574687e3
allenflo <allenflo@outlook.com>
2022-09-29 13:44:27 -0700

    Added 2nd Calculation

--- Projects/10_Calculator.py
+++ Projects/10_Calculator.py
@@ -1,10 +1,3 @@
-
-
-
-
-  
-
-
 #Add
 def add(n1, n2):
   return n1 + n2
@@ -28,18 +21,25 @@ operations = {
 "/": divide,
 }
 
+#Calculate First Answer
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number? "))
-
 for symbol in operations:
   print(symbol)
 operation_symbol = input("Pick an operation from the line above: ")
-
+num2 = int(input("What's the second number? "))
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
+#Print First Answer
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
+#second operation
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer, num3)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+
 
 
 
