0aa7edccdf90d0716e228bfa8e0d2e8ba0a189ab
allenflo <allenflo@outlook.com>
2022-10-24 09:50:16 -0700

    Working with no Functions

--- 100_Days_Python/Day12/GuessingGame/main.py
+++ 100_Days_Python/Day12/GuessingGame/main.py
@@ -9,7 +9,7 @@ number = random.randint(1, 101)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 print(f"Pssst, the correct answer is {number}")
-difficulty = input("Choose a difficulty.  Type 'easy' or 'hard':")
+difficulty = input("Choose a difficulty.  Type 'easy' or 'hard': ")
 
 if difficulty == "easy":
     tries = 10
@@ -17,16 +17,18 @@ else:
     tries = 5
 
 while tries > 0:
-    print(f"You have {number} attempts remaining to guess the number")
-    guess = input(int("Make a guess:"))
+    print(f"You have {tries} attempts remaining to guess the number")
+    guess = input("Make a guess: ")
+    guess = int(guess)
     if guess == number:
         print(f"You got it!  The answer was {number}")
+        tries = 0
     else:
         if guess < number:
             print("Too Low")
         else:
             print("Too High")
-    print("Guess again.")
+        print("Guess again.")
     tries -= 1
 
 
