9a8d8a84507dbe46d5d4f370145cefdcedb06d47
allenflo <allenflo@outlook.com>
2022-10-25 20:17:05 -0700

    CoffeeMachine

--- 100_Days_Python/Day14/HigherLower/main.py
+++ 100_Days_Python/Day14/HigherLower/main.py
@@ -2,27 +2,53 @@ from game_data import data
 from art import logo, vs
 import random
 
-#Pull Number of items out of database
+
+score = 0
+
+
+
+
+
+
+#Display Art
+print(logo)
+
+# Pull Number of items out of database
 total_items = (len(data))
-#Assign random choices to A and B
+
+# Assign random choices to A and B
 choice_a = random.randint(1, total_items)
 choice_b = random.randint(1, total_items)
-#Check to make sure choices are not equal or pick new choice
+# Check to make sure choices are not equal or pick new choice
 while choice_b == choice_a:
     choice_b = random.randint(1, total_items)
 
-print(logo)
 print(f"Compare A:{data[choice_a]['name']}, a {data[choice_a]['description']}, from {data[choice_a]['country']}")
 print(vs)
-print(f"Compare B:{data[choice_b]['name']}, a {data[choice_b]['description']}, from {data[choice_b]['country']}")
+print(f"Against B:{data[choice_b]['name']}, a {data[choice_b]['description']}, from {data[choice_b]['country']}")
+
+# Player Choice
+player_choice = input("Who has more followers?  Type 'A' or 'B': ")
+player_choice = player_choice.upper()
+
+# Get Followers
+followers_a = data[choice_a]['follower_count']
+followers_b = data[choice_b]['follower_count']
+
+# Check for winner
+winner = ""
+if followers_a > followers_b:
+    winner = "A"
+
+elif followers_b > followers_a:
+    winner = "B"
+    choice_a = choice_b
 
 
-
-
-
-# print(logo)
-# print(data[0]['name'])
-
-
-# print(vs)
-# print("test")
+if player_choice == winner:
+    print(logo)
+    score += 1
+    print(f"You're right!!!  Current score: {score}")
+else:
+    print(logo)
+    print(f"Sorry, that's wrong.  Final score: {score}")
---
+++ 100_Days_Python/Day15/main.py
@@ -0,0 +1,14 @@
+from coffeeMachine import MENU
+import random
+
+#TODO Prompt user 'What would you like? (espresso/latte/cappaccino:'
+
+#TODO Off will end execution of the program
+
+#TODO Print Report that shows values
+
+#TODO Check resources when user chooses a drink
+
+#TODO Process Coins....
+
+#TODO Check transaction successfull....
