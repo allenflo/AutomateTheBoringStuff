046e20d355231e6563db2da1fc9930ee339be95b
allenflo <allenflo@outlook.com>
2022-10-27 15:37:12 -0700

    Updated Directory

--- 100_Days_Python/Day1/1_1BandNameGenerator.py
+++
@@ -1,6 +0,0 @@
-# This program will generate a band name based on user input
-
-print("Hellow and welcome to Garage Band Name generator!!!!!")
-city = input("What city did you grow up in? ")
-petName= input ("What is the name of a pet? ")
-print("Your garage band name is " + city + " " + petName + "!!")
